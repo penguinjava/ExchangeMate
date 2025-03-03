85c5fb59b5d87b0f0128031e2736aa34b45bcac6
penguinjava <aiueoeom97@gmail.com>
2025-03-04 01:43:32 +0900

    new

---
+++ src/main/kotlin/com/jonghyeon/exchangemate/ExchangeMateApplication.kt
@@ -0,0 +1,11 @@
+package com.jonghyeon.exchangemate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExchangeMateApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExchangeMateApplication>(*args)
+}
